4aaaa78e549dd310104161c1f433a87559a43107
Mejinka <fidio50@gmail.com>
2023-05-25 17:22:54 +0100

    novo

--- lib/controlers/controladores.dart
+++ lib/controlers/controladores.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:intl/intl.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:provider/provider.dart';
 
-class Controllers {
-  final TextEditingController user = TextEditingController();
+import '../pages/first_page_extends/balanca.dart';
+
+class Controllers with ChangeNotifier {
+  final TextEditingController regCali = TextEditingController();
   final TextEditingController data = TextEditingController(
       text: DateFormat('dd/MM/yyyy').format(DateTime.now()));
   final TextEditingController certificado = TextEditingController();
@@ -57,27 +60,44 @@ class Controllers {
   final TextEditingController tempFinal = TextEditingController();
   final MaskTextInputFormatter tempFinalF =
       MaskTextInputFormatter(mask: '00.0', filter: {"0": RegExp(r'[0-9]')});
-}
 
-void clearAllFields(Controllers controllers) {
-  controllers.user.clear();
-  controllers.certificado.clear();
-  controllers.year.clear();
-  controllers.cliente.clear();
-  controllers.morada.clear();
-  controllers.morada2.clear();
-  controllers.objeto.clear();
-  controllers.marca.clear();
-  controllers.modelo.clear();
-  controllers.nserie.clear();
-  controllers.idinterna.clear();
-  controllers.altitude.clear();
-  controllers.latitude.clear();
-  controllers.cepController.clear();
-  controllers.cepController2.clear();
-  controllers.cimax.clear();
-  controllers.d.clear();
-  controllers.dT.clear();
-  controllers.tempInit.clear();
-  controllers.tempFinal.clear();
+  final TextEditingController horaInit = TextEditingController();
+  final MaskTextInputFormatter horaInitF =
+      MaskTextInputFormatter(mask: '00.0', filter: {"0": RegExp(r'[0-9]')});
+
+  final TextEditingController horaFinal = TextEditingController();
+  final MaskTextInputFormatter horaFinalF =
+      MaskTextInputFormatter(mask: '00.0', filter: {"0": RegExp(r'[0-9]')});
+
+  void clearAllFields() {
+    regCali.clear();
+    certificado.clear();
+    year.clear();
+    cliente.clear();
+    morada.clear();
+    morada2.clear();
+    objeto.clear();
+    marca.clear();
+    modelo.clear();
+    nserie.clear();
+    idinterna.clear();
+    altitude.clear();
+    latitude.clear();
+    cepController.clear();
+    cepController2.clear();
+    cimax.clear();
+    d.clear();
+    dT.clear();
+    tempInit.clear();
+    tempFinal.clear();
+    horaInit.clear();
+    horaFinal.clear();
+    switchbalanca;
+    notifyListeners();
+  }
+
+  void updateCimax(String value) {
+    cimax.text = value;
+    notifyListeners();
+  }
 }
---
+++ lib/controlers/time.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+
+import '../api.dart';
+
+class TimerManager extends ChangeNotifier {
+  Timer? _timer;
+  ValueNotifier<bool> isLoggedInStatus = ValueNotifier(false);
+
+  TimerManager() {
+    isLoggedInStatus.addListener(_onLoggedInStatusChanged);
+  }
+  void setLoggedInStatus(bool isLoggedIn) {
+    isLoggedInStatus.value = isLoggedIn;
+  }
+
+  void _onLoggedInStatusChanged() async {
+    if (!isLoggedInStatus.value) {
+      String? username = await getUsername();
+      print('O usuário $username deslogou');
+      stopTimer();
+    }
+  }
+
+  void startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 10), (Timer timer) async {
+      String? username = await getUsername();
+      print('o usuario $username' ' está logado');
+    });
+  }
+
+  void stopTimer() {
+    if (_timer != null) {
+      _timer!.cancel();
+      _timer = null;
+    }
+  }
+
+  @override
+  void dispose() {
+    stopTimer();
+    super.dispose();
+  }
+}
